79ff1712ebf3a0185594cf57cc9effd44eb628bb
Umendra Rajapakshe <umendrarajapakshe@gmail.com>
2020-01-12 14:38:10 +0530

    Add tester

---
+++ src/main/java/org/wso2/log4j/appenders/emailTester.java
@@ -0,0 +1,8 @@
+import org.apache.log4j.Logger;
+
+public class emailTester {
+    private static Logger log = Logger.getRootLogger();
+    public static void main(String [] args){
+        private Log
+    }
+}
