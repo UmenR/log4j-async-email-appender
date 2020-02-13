ecc736c47bc5422400b5a84cc977c12afc48243c
Umendra Rajapakshe <umendrarajapakshe@gmail.com>
2020-02-13 22:00:58 +0530

    Update and rename emailTester.java to EmailTester.java

--- src/main/java/org/wso2/log4j/appenders/emailTester.java
+++
@@ -1,8 +0,0 @@
-import org.apache.log4j.Logger;
-
-public class emailTester {
-    private static Logger log = Logger.getRootLogger();
-    public static void main(String [] args){
-        private Log
-    }
-}
